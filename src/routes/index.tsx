50dbfd0cb101b34a04df614082578544fde72dcc
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-10-12 18:44:05 +0200

    Clean up

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,9 @@
-import { Title } from "solid-start";
-import Counter from "~/components/Counter";
+import { A, Title } from "solid-start";
 
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <A href="/route-data">Route data</A>
     </main>
   );
 }
