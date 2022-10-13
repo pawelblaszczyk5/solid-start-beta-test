9e1c6f63de1d8598f2d0428d2657d90efc4eda9a
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-10-13 19:55:47 +0200

    Data refetching

--- src/routes/route-data.tsx
+++ src/routes/route-data.tsx
@@ -1,17 +1,35 @@
-import { useContext } from "solid-js";
-import { isServer } from "solid-js/web";
-import { useRouteData } from "solid-start";
-import { createServerData$, ServerContext } from "solid-start/server";
+import { startTransition } from "solid-js";
+import { refetchRouteData, useRouteData } from "solid-start";
+import { createServerData$ } from "solid-start/server";
 
 export const routeData = () =>
-  createServerData$((_, event) => {
-    return { test: 5 };
-  });
+  createServerData$(
+    async () => {
+      await new Promise<void>((res) => setTimeout(res, 1000));
+      return { test: Math.random() };
+    },
+    { key: "test" }
+  );
 
 const Screen = () => {
   const data = useRouteData<typeof routeData>();
 
-  return <h1>Test {data()?.test}</h1>;
+  const refetchData = () => {
+    refetchRouteData("test");
+  };
+
+  return (
+    <>
+      <h1>Test {data.latest?.test}</h1>
+      <button
+        class="py-2 px-4 bg-emerald-400 text-black font-semibold disabled:opacity-40"
+        onClick={refetchData}
+        disabled={data.state === "refreshing"}
+      >
+        Refetch
+      </button>
+    </>
+  );
 };
 
 export default Screen;
