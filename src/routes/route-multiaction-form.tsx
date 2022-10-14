6f55fc3250d9c6e7a540a2cda88c4a6062966455
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-10-14 15:25:23 +0200

    Route multiaction form test

--- src/routes/route-multiaction-form.tsx
+++ src/routes/route-multiaction-form.tsx
@@ -1,10 +1,16 @@
 import { Component, createEffect, createMemo, For, on } from "solid-js";
+import { FormError } from "solid-start";
 import { createServerMultiAction$ } from "solid-start/server";
 
 const Screen: Component = () => {
   // It's currently slightly bugged - https://github.com/solidjs/solid-start/issues/310
   const [data, trigger] = createServerMultiAction$(async (values: FormData) => {
     await new Promise<void>((res) => setTimeout(res, 1000));
+
+    if (Math.random() > 0.5) {
+      throw new FormError("test");
+    }
+
     return { test: 5 };
   });
 
@@ -14,7 +20,20 @@ const Screen: Component = () => {
         <input class="border-2 border-dark-400" name="test" />
       </trigger.Form>
       <For each={data}>
-        {(object) => <div>{() => object.result?.test ?? "Pending"}</div>}
+        {(object) => (
+          <div>
+            <span>
+              {() =>
+                object.result
+                  ? object.result.test
+                  : object.error
+                  ? "Error"
+                  : "Pending"
+              }
+            </span>{" "}
+            <button onClick={object.retry}>Resend</button>
+          </div>
+        )}
       </For>
     </>
   );
