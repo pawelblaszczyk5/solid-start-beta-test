541792524ea6353d93c5bcf8987c5e1907e765ef
pawelblaszczyk5 <pawelblaszczyk@wir.pl>
2022-10-15 20:14:57 +0200

    Props mutation test

--- src/routes/docs.tsx
+++ src/routes/docs.tsx
@@ -1,10 +1,19 @@
-const Button = () => <button>Test button</button>;
+import { createSignal } from "solid-js";
+
+const Button = (props: { text: string; onClick: () => void }) => (
+  <button onClick={props.onClick}>Button {props.text}</button>
+);
 
 const Screen = () => {
+  const [something, setSomething] = createSignal("");
+
   return (
     <div>
       <iframe data-why>
-        <Button />
+        <Button
+          onClick={() => setSomething((s) => s + s.length)}
+          text={something()}
+        />
       </iframe>
     </div>
   );
